02f1b1d2e623bf4ffbfa7501db545d40d953affe
samirkhan10 <pkan5320@gmail.com>
2023-08-14 21:30:40 +0530

    new UI added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Live Streaming Kit Demo Android
+
+This is a sample Demo of the article [How To Build Live Streaming Services With Android UIKit](https://www.zegocloud.com/blog/live-streaming-services-with-android). For more details, please read the article.
---
+++ app/src/main/java/com/zegocloud/uikit/prebuilt/livestreamingexample/LiveActivity.java
@@ -0,0 +1,40 @@
+package com.zegocloud.uikit.prebuilt.livestreamingexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.zegocloud.uikit.prebuilt.livestreaming.ZegoUIKitPrebuiltLiveStreamingConfig;
+import com.zegocloud.uikit.prebuilt.livestreaming.ZegoUIKitPrebuiltLiveStreamingFragment;
+
+public class LiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live);
+
+        addFragment();
+    }
+
+    private void addFragment() {
+        long appID = getIntent().getLongExtra("appID", 0L);
+        String appSign = getIntent().getStringExtra("appSign");
+        String userID = getIntent().getStringExtra("userID");
+        String userName = getIntent().getStringExtra("userName");
+
+        boolean isHost = getIntent().getBooleanExtra("host", false);
+        String liveID = getIntent().getStringExtra("liveID");
+
+        ZegoUIKitPrebuiltLiveStreamingConfig config;
+        if (isHost) {
+            config = ZegoUIKitPrebuiltLiveStreamingConfig.host();
+        } else {
+            config =  ZegoUIKitPrebuiltLiveStreamingConfig.audience();
+        }
+
+        ZegoUIKitPrebuiltLiveStreamingFragment fragment = ZegoUIKitPrebuiltLiveStreamingFragment.newInstance(
+            appID, appSign, userID, userName, liveID, config);
+        getSupportFragmentManager().beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commitNow();
+    }
+}
